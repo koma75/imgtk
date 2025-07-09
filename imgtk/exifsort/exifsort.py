38ec65e53b51c9339a946d122b13c8ee5a0b2abb
koma75 <okunoya@path-works.net>
2025-07-10 01:24:32 +0900

    update Pillow to 11.3 and update to new getexif()

--- imgtk/exifsort/exifsort.py
+++ imgtk/exifsort/exifsort.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # BSD 2-Clause License
-# 
+#
 # Copyright (c) 2023 koma <okunoya@path-works.net>
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright notice, this
 #    list of conditions and the following disclaimer.
-# 
+#
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -187,11 +187,12 @@ def getDateOfImage(fpath, verbose=False):
     # First try to get date from exif
     try:
         img = Image.open(fpath)
-        exif = img._getexif()
+        exif = img.getexif()
         try:
             for id,val in exif.items():
                 tg = TAGS.get(id,id)
-                if tg == "DateTimeOriginal":
+                pout(f"{id}({tg}): {val}", verbose, Level.DEBUG)
+                if tg == "DateTimeOriginal" or tg == "DateTime":
                     dtArr = re.split('[ :]', val)
                     rt = date(int(dtArr[0]), int(dtArr[1]), int(dtArr[2]))
             img.close()
